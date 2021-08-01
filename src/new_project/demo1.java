5d7a351f9b442b442f113f8ff226cb5efd865136
Admin <Admin@Admin-PC>
2021-08-01 14:19:19 +0530

    [swapnil]:updated new project

---
+++ src/new_project/demo1.java
@@ -0,0 +1,10 @@
+package new_project;
+
+public class demo1 {
+	public static void main(String[] args) {
+		
+		
+		System.out.println("new class 1 ");
+	}
+
+}
---
+++ src/new_project/demo2.java
@@ -0,0 +1,11 @@
+package new_project;
+
+public class demo2 
+{
+	public static void main(String[] args) {
+		
+		
+		System.out.println("new class 2 ");
+	}
+
+}
